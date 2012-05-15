1547d3ea67ab9b780178f4b5297d24b18b7a7b03
Noe Podestá <podesta.noe@gmail.com>
2012-05-15 15:34:56 -0300

    Fire Result change

--- app/models/FireResult.java
+++ app/models/FireResult.java
@@ -1,12 +1,6 @@
 package models;
 
-/**
- * Created by IntelliJ IDEA.
- * User: NoePodesta
- * Date: 15/05/12
- * Time: 10:55
- * To change this template use File | Settings | File Templates.
- */
+
 public enum FireResult {
 
     WATER("Water, you missed", "Calm, it was water!"),
@@ -14,11 +8,6 @@ public enum FireResult {
     HIT("You hit it", "Ohh noo, your ship was hit!"),
     ACTIVE("You already shot here","They already shot here!"),
     WIN("Good, you win the game!", "HA HA, you are a looser!");
-
-
-
-
-
 
     private String currentPlayerMessage;
     private String opponentMessage;
