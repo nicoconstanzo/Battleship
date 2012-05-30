830890077efc4fadb251aaf1fe3365f96ab8124b
Nico Constanzo <nicoconstanzo@gmail.com>
2012-05-29 21:39:13 -0300

    MAJOR changes to StrategyRoom. - Changed how positions are handled - Changes to strategyRoom & functions.js

--- public/javascripts/functions.js
+++ public/javascripts/functions.js
@@ -1,40 +1,67 @@
 var value = 0;
-$( "#aircraftCarrier, #battleship, #destroyer, #patrolShip, #submarine" ).draggable({
-    revert: "invalid",
-    snap: '#strategyBoard .boardBody'
+
+
+
+$("#aircraftCarrier, #battleship, #destroyer, #patrolShip, #submarine").draggable({
+    revert:"invalid",
+    snap:'#strategyBoard .boardBody'
 });
+
 $("#aircraftCarrier, #submarine, #patrolShip, #destroyer, #battleship").rotate({
-   bind:
-     {
-           click: function(){
-               value +=90;
-               $(this).rotate({ animateTo:value})
-           }
+    bind:{
+        click:function () {
+            value += 90;
+            $(this).rotate({ animateTo:value})
+        }
+    }
+
+});
+
+$("#strategyBoard .boardBody").droppable({
+    addClasses: false,
+    drop:function (event, ui) {
+
+        var target = event.target;
+        $(target).css('background', 'blue')
+
+//        var targetClass = $(target).attr('class').split(" ")
+//        var yPosition = targetClass[0]
+//        var xPosition = targetClass[1]
+
+        var shipType = $(ui.draggable).attr('id')
+
+        var xPosition = $(target).data('x')
+        var yPosition = $(target).data('y')
+
+        console.log(xPosition,yPosition, shipType)
+
+        if(shipType == "destroyer" || shipType == "patrolShip") {
+            var length = 2;
+
         }
 
-});
+        if(shipType == "aircraftCarrier") {
+            var length = 5
 
+        }
+        if(shipType == "submarine") {
+            var length = 3
 
-$( "#strategyBoard .boardBody" ).droppable({
+        }
+        if(shipType == "battleship") {
+            var length = 4
 
-   drop: function(event, ui) {
+        }
 
-    var position=event.target.className;
-    console.log(position);
     },
-     hoverClass: "hoverClass"
+    hoverClass:"hoverClass"
 });
 
 
-function resetMessage(){
-    var message =  $(message);
-    message.removeChild(message.getElementsByTagName("p")[0]);
 
+
+function resetMessage() {
+    var message = $(message);
+    message.removeChild(message.getElementsByTagName("p")[0]);
 }
 
-$('document').ready(function(){
-
-
- $(".myPiece").multiDraggable({ group: $(".myPiece"), snap: '#strategyBoard .boardBody', dragNative : function () {}});
-
-});
